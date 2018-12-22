3e37f7f05731996674d31a8ab6182a09c1b77c23
Neil Pankey <npankey@gmail.com>
2018-12-21 20:44:08 -0500

    use orientation to draw the box and center

--- main.go
+++ main.go
@@ -68,12 +68,6 @@ func Dump(s *Session) {
 
 	for _, p := range photos {
 		faces, err := p.Faces(s)
-		if len(faces) == 0 {
-			continue
-		}
-		if p.Orientation != 3 {
-			continue
-		}
 		fmt.Printf("%s orientation=%d type=%d adj=%t\n", p.Path, p.Orientation, p.Type, p.HasAdjustments)
 		for _, f := range faces {
 			fmt.Printf("  %s center=(%f,%f) size=%f left=(%f,%f) right=(%f,%f), mouth=(%f,%f)\n",
@@ -81,14 +75,12 @@ func Dump(s *Session) {
 		}
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
-			break // TODO
 		}
 
 		// Quick check if things are working...
 		if len(faces) > 0 {
 			if err := OutlineFaces(s, p); err != nil {
 				fmt.Fprintln(os.Stderr, err)
-				break // TODO
 			}
 		}
 	}
@@ -225,22 +217,17 @@ func OutlineFaces(s *Session, p *Photo) error {
 	draw.Draw(dst, r, src, image.ZP, draw.Src)
 
 	border := 10
-	dx, dy := float64(r.Dx()), float64(r.Dy())
 	for _, f := range faces {
 		// Trial and error suggests f.Size is a rough radius
 		// about the center of the face. A box is close enough
 		// for validating.
-		minX := int((f.CenterX - f.Size) * dx)
-		maxX := int((f.CenterX + f.Size) * dx)
-		minY := int(((f.CenterY) - f.Size) * dy)
-		maxY := int(((f.CenterY) + f.Size) * dy)
+		min := makePoint(f.CenterX-f.Size, f.CenterY-f.Size, r, p.Orientation)
+		max := makePoint(f.CenterX+f.Size, f.CenterY+f.Size, r, p.Orientation)
 
-		//minX, minY, maxX, maxY = minY, minX, maxY, maxX
-
-		top := image.Rect(minX-border, minY-border, maxX+border, minY)
-		bot := image.Rect(minX-border, maxY, maxX+border, maxY+border)
-		left := image.Rect(minX-border, minY, minX, maxY)
-		right := image.Rect(maxX, minY, maxX+border, maxY)
+		top := image.Rect(min.X-border, min.Y-border, max.X+border, min.Y)
+		bot := image.Rect(min.X-border, max.Y, max.X+border, max.Y+border)
+		left := image.Rect(min.X-border, min.Y, min.X, max.Y)
+		right := image.Rect(max.X, min.Y, max.X+border, max.Y)
 
 		draw.Draw(dst, top, blue, image.ZP, draw.Src)
 		draw.Draw(dst, bot, green, image.ZP, draw.Src)
@@ -250,6 +237,7 @@ func OutlineFaces(s *Session, p *Photo) error {
 		drawDot(dst, f.LeftEyeX, f.LeftEyeY, r, p.Orientation, blue)
 		drawDot(dst, f.RightEyeX, f.RightEyeY, r, p.Orientation, red)
 		drawDot(dst, f.MouthX, f.MouthY, r, p.Orientation, green)
+		drawDot(dst, f.CenterX, f.CenterY, r, p.Orientation, black)
 	}
 
 	// Dump the files on disk for inspection
@@ -293,4 +281,5 @@ var (
 	green = &image.Uniform{color.RGBA{0, 255, 0, 255}}
 	blue  = &image.Uniform{color.RGBA{0, 0, 255, 255}}
 	gray  = &image.Uniform{color.RGBA{100, 100, 100, 255}}
+	black = &image.Uniform{color.RGBA{0, 0, 0, 255}}
 )
